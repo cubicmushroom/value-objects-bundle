c9046cd372cb34ae5b73c75255b26ebab38eebc7
Toby Griffiths <toby@cubicmushroom.co.uk>
2015-10-18 16:31:33 +0100

    Added separate forms.xml for form services

--- src/DependencyInjection/CMValueObjectsExtension.php
+++ src/DependencyInjection/CMValueObjectsExtension.php
@@ -23,6 +23,7 @@ class CMValueObjectsExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('forms.xml');
         $loader->load('services.xml');
     }
 }
